a08f6a9855e4696b028a6e33d9a6f0f7bc7a11ac
khanhdoanvja <khanhdoan.it@gmail.com>
2021-08-02 23:30:18 +0700

    Update specs

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'covid-vaccination'`, () => {
+  it(`should have as title 'vja-covid-vaccination'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('covid-vaccination');
+    expect(app.title).toEqual('vja-covid-vaccination');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('covid-vaccination app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('vja-covid-vaccination app is running!');
   });
 });
